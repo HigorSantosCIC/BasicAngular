4f6906fdf093a2fabbe3c9e05c39eee62d66f9cc
Higor Santos <higorsantos.cic.unb@gmail.com>
2021-09-09 17:01:52 -0300

    Templates and Interpolation, Interact the component with HTML

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,11 +2,27 @@ import{ Component } from '@angular/core'
 
 @Component({
   selector: 'hello',
-  template: `
-      <h1> Hello World </h1>
-  `
+  templateUrl: './hello.component.html'
 })
 
 export class HelloComponent {
 
+  nome: string;
+  clientes: Cliente[];
+
+  constructor() {
+    this.nome = 'Higor';
+    let fulano = new Cliente('Higor', 23);
+    let cicraco = new Cliente('Cicrano',33);
+    let outro = new Cliente('rapaz', 110);
+    this.clientes = [fulano, cicraco, outro];
+
+  }
+}
+
+class Cliente{
+  constructor(
+    public nome: string,
+    public idade: number
+    ){}
 }
